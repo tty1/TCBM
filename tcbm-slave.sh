c6de1ce5874613117fa7a515da67a14d7f8ba0ce
tty1 <mail@thomas-rainer.at>
2017-11-27 12:17:29 +0100

    Add files via upload

--- tcbm-slave.sh
+++ tcbm-slave.sh
@@ -1,27 +1,71 @@
 #!/bin/bash
 
-#cd ~
-#mkdir .tcbm
-#cd .tcbm
-#mkdir client
-#cd client
-#mkdir tmp
-#cd tmp
-#mkdir job
+	#Tom's Cluster Batch Manager (slave)
+	#Version 0.06
+
+		TMP_LOCATION= $(sed -n '2{p;q}' slave_config.txt)
+		CONFIG_LOCATION=~/.config/tcbm/slave/
+		USE_THIS_SLAVE=`<"$CONFIG_LOCATION"use_this_slave`
+		LANGUAGE=de
+		MAX_CPU_LOAD=	# dont start a job if the CPU-load is over nn$
+		MAX_MEM_LOAD=	# dont start a job if the memory-load is over
+		MIN_LOAD=
+		MAX_LOAD_TIME=
+		MIN_LOAD_TIME=
+		PROCESSING=No #(noch zu programmieren ist nur ein vorübergender platzhalter)
+		LAST_SLAVELOG="STARTUPSLAVE" #text für logfile bei Computerneustart
+
+		typeset -i MEMTOTAL MEMFREE PROZENT PROZENT_FREE MEMAV MEM
+		MEMTOTAL=`cat /proc/meminfo | grep MemTotal* | sed -n s/\ /"\n"/gp | grep '[0-9]'`
+		MEMFREE=`cat /proc/meminfo | grep MemFree* | sed -n s/\ /"\n"/gp | grep '[0-9]'`
+		MEMAV=`cat /proc/meminfo | grep MemAvailable* | sed -n s/\ /"\n"/gp | grep '[0-9]'`
+		MEM=$MEMAV+$MEMFREE
+		PROZENT_FREE=100*$MEM/$MEMTOTAL
+		PROZENT=100-$PROZENT_FREE
+		echo $PROZENT
+
+
+cd ~
+mkdir .tcbm
+cd .tcbm
+mkdir slave
+cd slave
+mkdir tmp
+cd tmp
+mkdir job
 
 USE_THIS_SLAVE=Yes #use this slave Yes/No
 PROCESSING=No #(noch zu programmieren ist nur ein vorübergender platzhalter)
 LAST_SLAVELOG="STARTUPSLAVE" #text für logfile bei Computerneustart
 
+#rm ~/.tcbm/slave/last_slavelog.txt
+#rm ~/.tcbm/slave/slavelog.txt #nur vorübergehend zur fehlersuche
 
-rm ~/.tcbm/slave/last_slavelog.txt      dsfsd
-rm ~/.tcbm/slave/slavelog.txt #nur vorübergehend zur fehlersuche
+case $1 in
+	--help)		cat ~/opt/tcbm/files/help.txt
+	;;
+	--version)
+	#version $(sed -n '2{p;q}' tcbm.sh)
+	echo "VERSION"
+	echo $VERSION
+	;;
+	--1st)		echo first_setup
+	;;
+	--config)
+		echo setup
 
+	;;
+	--stop)		echo stop
+	;;
+	--start)	echo start_the_rest
 	echo $LAST_SLAVELOG >> ~/.tcbm/slave/last_slavelog.txt
-
-	while [[ 1 == 1 ]]; do
+	while [[ 1 == 1 ]]; do	# --start
 		OLDDATE=`date`    #starting time of current status
 		LAST_SLAVELOG=`<~/.tcbm/slave/last_slavelog.txt`
+
+		#top bla bla bla
+		#PROCESSING=Yes/NO
+
 		if [[ $LAST_SLAVELOG == STARTUPSLAVE ]]; then                                  #eventuel noch durch if; elif; fi ersetzen
 			echo $LAST_SLAVELOG
 			echo >> ~/.tcbm/slave/slavelog.txt
@@ -44,14 +88,15 @@ rm ~/.tcbm/slave/slavelog.txt #nur vorübergehend zur fehlersuche
 
 			if ! [[ -f ~/.tcbm/slave/tmp/job.tcbm ]]; then    #test if a new jobfile is not available
 				while ! [[ -f ~/.tcbm/slave/tmp/job.tcbm ]]; do  #if no new jobfile is available set status to standby
-					echo standby > ~/.tcbm/slave/slavestatus.txt
 					NEWDATE=`date`    #ending time of current status
+					echo standby > ~/.tcbm/slave/slavestatus.txt
 					echo standby $OLDDATE $NEWDATE > ~/.tcbm/slave/last_slavelog.txt
-					sleep 1
+					sleep 5
 				done
 
 			elif [[ -f ~/.tcbm/slave/tmp/job.tcbm ]]; then
-				while [[ 1 == 1 ]]; do
+				while [[ -f ~/.tcbm/slave/tmp/job.tcbm ]]; do
+				#while [[ 1 == 1 ]]; do
 					echo executejob > ~/.tcbm/slave/slavestatus.txt
 					sleep 3 #wird noch durch entsprechenden Befehl ersetzt
 					NEWDATE=`date`
@@ -63,7 +108,7 @@ rm ~/.tcbm/slave/slavelog.txt #nur vorübergehend zur fehlersuche
 			fi
 
 		elif [[ $USE_THIS_SLAVE == Yes ]] && [[ $PROCESSING == Yes ]]; then
-			echo at_work > slavestatus.txt
+			echo "the system is used for an other task" > slavestatus.txt
 			# render
 
 		elif [[ $USE_THIS_SLAVE == No ]] && [[ $PROCESSING == Yes ]]; then
@@ -78,3 +123,7 @@ rm ~/.tcbm/slave/slavelog.txt #nur vorübergehend zur fehlersuche
 		fi
 
 	done
+
+	;;
+	*)				echo "--help and you got more information"
+esac
